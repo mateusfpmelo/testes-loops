2812e8c1b15c859c1f2faeac9a83b32b95a551ff
Mateus <mateusfpmelodev@gmail.com>
2022-02-04 16:01:47 -0300

    initial commit

---
+++ testes.js
@@ -0,0 +1,10 @@
+
+const beleza = true;
+const rica = true; 
+const idade = 22;
+
+if (idade >= 18 && beleza == true && rica == true) {
+    console.log("Você foi selecionade!")
+} else {
+    console.log("Infelizmente você não foi selecionada desta vez!")
+}
